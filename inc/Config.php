f08c103e1ae9f3c710d44eb07e39e42ce66ac5f0
Luis <dat.tm24@gmail.com>
2019-08-24 00:33:15 +0000

    add generic name for config

--- inc/Config.php
+++ inc/Config.php
@@ -6,14 +6,12 @@
  * Time: 8:57 AM
  */
 namespace BinaryCarpenter\PLUGIN_NS;
-class Config;
+class Config
 {
-    const PLUGIN_NAME = 'BC Menu Bar Cart';
-    const PLUGIN_MENU_NAME = 'BC Menu Cart';
-    const PLUGIN_SLUG = 'bc_menu_bar_cart';
-    const PLUGIN_TEXT_DOMAIN = 'bc-menu-bar-cart-tx';
-    const PLUGIN_OPTION_NAME = 'bc_menu_bar_cart_option_name';
-    const PLUGIN_COMMON_HANDLER = 'bc_menu_bar_cart_handler';
+    const NAME = 'Starter plugin';
+    const MENU_NAME = 'Starter plugin';
+    const SLUG = 'starter_plugin';
+    const TEXT_DOMAIN = 'starter-plugin-tx';
     const IS_FREE = false;//is free or pro
-    const KEY_CHECK_OPTION = 'bc_menu_cart_key_check';
+    const KEY_CHECK_OPTION = 'starter_key_check';//unique across plugins
 }
