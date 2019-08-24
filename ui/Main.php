3581f7f117bc64b125b8507b6f1b5f5f646655f0
Luis <dat.tm24@gmail.com>
2019-08-24 15:04:27 +0000

    add demo content for main UI

--- ui/Main.php
+++ ui/Main.php
@@ -10,10 +10,27 @@ class Main
 {
     public static function ui()
     {
-        $form = new Form('test_option', 0);
+        $first_tab = array(
+            'title' => 'First tab',
+            'content' => array(
+                'first row'
+                ),
+             'is_active' => false,
+             'is_disabled' => false
+             );
+        $second_tab = array(
+            'title' => 'Second tab',
+            'content' => array(
+                'first row'
+                ),
+             'is_active' => true,
+             'is_disabled' => false
+             );
 
-        $form->notice('Help me!', 'notice', true, true);
-
+        UI::open_root();
+//        $form = new Form('test_option', 0);
+        UI::tabs(array($first_tab, $second_tab), true);
+        UI::close_root();
     }
 
 }
